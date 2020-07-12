2660fc5492e95231443b5cbc6c33d600c8ef0610
MattGson <matt.goodson.business@gmail.com>
2020-07-13 09:14:56 +1200

    Updated build

--- build/src/Client/index.d.ts
+++ build/src/Client/index.d.ts
@@ -1,3 +1,6 @@
 import Knex = require('knex');
 export declare const initialize: (knex: Knex) => void;
 export declare const knex: () => Knex<any, unknown[]>;
+export * from './QueryBuilders/Loaders';
+export * from './QueryBuilders/Persistors';
+export * from './QueryBuilders/Updaters';
--- build/src/Client/index.js
+++ build/src/Client/index.js
@@ -1,4 +1,14 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !exports.hasOwnProperty(p)) __createBinding(exports, m, p);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.knex = exports.initialize = void 0;
 const knex_duplicate_key_1 = require("./lib/knex.duplicate.key");
@@ -9,7 +19,7 @@ global._logger = {
     error: console.error,
 };
 const state = {
-    knex: undefined
+    knex: undefined,
 };
 exports.initialize = (knex) => {
     state.knex = knex;
@@ -20,4 +30,7 @@ exports.knex = () => {
         throw new Error('Nodent must be configured with a knex instance');
     return state.knex;
 };
+__exportStar(require("./QueryBuilders/Loaders"), exports);
+__exportStar(require("./QueryBuilders/Persistors"), exports);
+__exportStar(require("./QueryBuilders/Updaters"), exports);
 //# sourceMappingURL=index.js.map
