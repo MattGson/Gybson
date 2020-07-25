d1b71edef1ebb2725f874b1c96bb0212e4c009d8
MattGson <matt.goodson.business@gmail.com>
2020-07-25 17:34:17 +1200

    Updated package version

--- test/test.ts
+++ test/test.ts
@@ -1,20 +1,20 @@
 import Gybson, { LogLevel } from '../build/src/Client';
 import by from '../Gen';
 
+Gybson.init({
+    client: 'mysql',
+    connection: {
+        host: '127.0.0.1',
+        port: 3306,
+        database: 'komodo',
+    },
+    config: {
+        logLevel: LogLevel.debug,
+    },
+});
+
 const main = async () => {
-    Gybson.init({
-        client: 'mysql',
-        connection: {
-            host: '127.0.0.1',
-            port: 3306,
-            database: 'komodo',
-            timezone: 'utc',
-            charset: 'utf8mb4',
-        },
-        config: {
-            logLevel: LogLevel.debug,
-        },
-    });
+
 
     const gyb = by();
 
@@ -35,4 +35,4 @@ const main = async () => {
     });
 };
 
-main();
+main().then(() => console.log('done'));
