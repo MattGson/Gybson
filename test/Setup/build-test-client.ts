c7a680565f6564e158318467b88b7d6e9dc86f96
MattGson <matt.goodson.business@gmail.com>
2020-08-01 19:11:29 +1200

    Fixed test startup script

--- test/Setup/build-test-client.ts
+++ test/Setup/build-test-client.ts
@@ -1,7 +1,12 @@
 import { generate } from '../../src/Generator';
-import { connection } from './buildMySQL';
+import { buildMySQLSchema, closeConnection, connection } from './buildMySQL';
 import path from 'path';
 
 const out = path.join(__dirname, '../Gen');
 const lib = path.join(__dirname, '../../src/Client');
-generate(connection, out, lib).catch((e) => console.log(e));
+
+// build a db and gybson client for testing
+buildMySQLSchema()
+    .then(() => closeConnection())
+    .then(() => generate(connection, out, lib))
+    .catch((e) => console.log(e));
