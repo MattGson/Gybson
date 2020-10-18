80f480218d018faab6cecaac80f01363040a94bb
MattGson <matt.goodson.business@gmail.com>
2020-10-18 16:51:41 +1300

    Fixed connection typing

--- test/Setup/buildMySQL.ts
+++ test/Setup/buildMySQL.ts
@@ -1,11 +1,12 @@
 import Knex from 'knex';
 import { buildSchema } from './buildSchema';
+import { Connection } from '../../src/Generator/Introspection';
 import { Connection as PGConn } from 'pg-promise/typescript/pg-subset';
 import { Connection as MySQLConn } from 'promise-mysql';
-type Connection = PGConn | MySQLConn;
+type PoolConnection = PGConn | MySQLConn;
 
-export const schemaName = 'gybson_test';
-export const connection = {
+export const schemaName = 'komodo';
+export const connection: Connection = {
     client: 'mysql',
     connection: {
         host: 'localhost',
@@ -23,7 +24,7 @@ const state: any = {
 
 // helpers for testing manual connection handling
 export const getPoolConnection = async () => state.knex.client.acquireConnection();
-export const closePoolConnection = async (connection: Connection) => state.knex.client.releaseConnection(connection);
+export const closePoolConnection = async (connection: PoolConnection) => state.knex.client.releaseConnection(connection);
 
 export const knex = (): Knex => state.knex;
 export const closeConnection = async () => state.knex.destroy();
