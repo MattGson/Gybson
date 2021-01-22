03e56c705724c859aa69b722891305181f64d0ce
MattGson <matt.goodson.business@gmail.com>
2021-01-22 22:42:20 +1300

    Fixed transaction

--- src/Client/QueryBuilders/Transaction.ts
+++ src/Client/QueryBuilders/Transaction.ts
@@ -1,32 +1,30 @@
-import {knex} from '../index';
-import {Connection as PGConn} from 'pg';
-import {Connection as MySQLConn} from 'promise-mysql';
-import {logger} from '../lib/logging';
+import { knex } from '../index';
+import { Connection as PGConn } from 'pg';
+import { Connection as MySQLConn } from 'promise-mysql';
+import { logger } from '../lib/logging';
 
 type Connection = PGConn | MySQLConn;
 
 /**
  * run a function with parameters as a single database transaction
  * //TODO - postgres compatibility check
+ * // TODO:- cannot release connections - careful of memory
  * @param fn, a function that executes queries, takes arguments (connection, params)
  * @return Promise<any>
  */
 export const runTransaction = async <T>(fn: (conn: Connection) => T): Promise<T> => {
-    let conn = knex().client.acquireConnection();
+    let conn = await knex().client.acquireConnection();
 
     try {
-        conn = await knex().client.acquireConnection().getConnection();
-        await conn.query('START TRANSACTION');
-
+        await knex().raw('START TRANSACTION').connection(conn);
         const result = await fn(conn);
-
-        await conn.query('COMMIT');
-        conn.release();
+        await knex().raw('COMMIT').connection(conn);
+        // conn.release();
         return result;
     } catch (err) {
         logger().debug('Transaction failed. Rolling back...');
-        conn.query('ROLLBACK');
-        conn.release();
+        await knex().raw('ROLLBACK').connection(conn);
+        // conn.release();
         throw err;
     }
 };
